221a3df4c9f2815a324af0b493cfea9ee05766d5
anushmauranga <Intel@123>
2017-10-08 13:49:58 +0530

    Intial Project

---
+++ warehouse-inventory System/warehouse-inventory system/home.php
@@ -0,0 +1,31 @@
+<?php
+  $page_title = 'Home Page';
+  require_once('includes/load.php');
+  if (!$session->isUserLoggedIn(true)) { redirect('index.php', false);}
+?>
+<?php include_once('layouts/header.php'); ?>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Page Header
+        <small>Optional descriptions</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i>Level
+            </a>
+        </li>
+        <li class="active">Here</li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content container-fluid">
+
+    <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+
+</section>
+<?php include_once('layouts/footer.php'); ?>
