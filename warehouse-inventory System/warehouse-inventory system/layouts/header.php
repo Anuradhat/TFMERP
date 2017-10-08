13c3c38b78d1e8acc4bd3e4d5f794bf58b0ca8e8
Anuradhat <anuradhath@live.com>
2017-10-08 17:13:35 +0530

    no message

--- warehouse-inventory System/warehouse-inventory system/layouts/header.php
+++ warehouse-inventory System/warehouse-inventory system/layouts/header.php
@@ -60,9 +60,9 @@ desired effect
             <!-- Logo -->
             <a href="index2.html" class="logo">
                 <!-- mini logo for sidebar mini 50x50 pixels -->
-                <span class="logo-mini"><b>T</b>ER</span>
+                <span class="logo-mini"><b>TFM ERP</b></span>
                 <!-- logo for regular state and mobile devices -->
-                <span class="logo-lg"><b>TFM</b>ERP</span>
+                <span class="logo-lg"><b>TFM ERP</b></span>
             </a>
             <!-- Header Navbar -->
             <nav class="navbar navbar-static-top" role="navigation">
