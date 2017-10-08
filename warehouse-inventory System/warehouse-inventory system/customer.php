18a6d41a210d712e31e34ba27a3e6c676d9df237
anushmauranga <Intel@123>
2017-10-08 15:47:06 +0530

    no message

---
+++ warehouse-inventory System/warehouse-inventory system/customer.php
@@ -0,0 +1,109 @@
+<?php
+$page_title = 'All categories';
+require_once('includes/load.php');
+page_require_level(1);
+
+$all_customers = find_by_sql("call spSelectAllCustomers();")
+?>
+
+
+<?php include_once('layouts/header.php'); ?>
+<section class="content-header">
+    <h1>
+        Customer Master
+        <small>Optional description</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i>Master
+            </a>
+        </li>
+        <li class="active">Cutomer</li>
+    </ol>
+    <style>
+        form {
+            display: inline;
+        }
+    </style>
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <!-- Your Page Content Here -->
+    <div class="box box-default">
+        <div class="box-header with-border">
+            <h3 class="box-title">Cutomer Details</h3>
+
+            <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse">
+                    <i class="fa fa-minus"></i>
+                </button>
+            </div>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <div class="row">
+                <div class="col-xs-12">
+
+                    <div class="box">
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            <table id="table" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Action</th>
+                                        <th>Customer Code</th>
+                                        <th>Customer Name</th>
+                                        <th>Address</th>
+                                        <th>Telephone</th>
+                                        <th>Fax</th>
+                                        <th>E-Mail</th>
+                                        <th>Contact Person</th>
+                                        <th>VAT</th>
+                                        <th>SVAT</th>
+                                        <th>Credit Period</th>
+                                        <th>Sales Person</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($all_customers as $cus):?>
+                                    <tr>
+                                        <td>
+                                            <div class="form-group">
+                                                <form method="post" action="add_customer.php">
+                                                    <button type="submit" class="btn  btn-info btn-xs glyphicon glyphicon-edit"></button>
+                                                </form>
+                                                <form method="post" action="delete_customer.php">
+                                                    <button type="submit" class="btn btn-danger btn-xs glyphicon glyphicon-trash" data-toggle="confirmation"></button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                        <td><?php echo remove_junk($cus['CustomerCode']); ?></td>
+                                        <td><?php echo remove_junk(ucfirst($cus['CustomerName'])); ?></td>
+                                        <td><?php echo remove_junk(ucfirst($cus['DeliveryAddress1']).','.ucfirst($cus['DeliveryAddress2']).ucfirst($cus['DeliveryAddress3'])); ?></td>
+                                        <td><?php echo remove_junk($cus['Tel']); ?></td>
+                                        <td><?php echo remove_junk($cus['Fax']); ?></td>
+                                        <td><?php echo remove_junk($cus['Email']); ?> </td>
+                                        <td><?php echo remove_junk(ucfirst($cus['ContactPerson'])); ?></td>
+                                        <td><?php echo remove_junk($cus['VATNo']); ?></td>
+                                        <td> <?php echo remove_junk($cus['SVATNo']); ?></td>
+                                        <td> <?php echo remove_junk($cus['CreditPeriod']); ?></td>
+                                        <td><?php echo remove_junk(ucfirst($cus['SalesPersonCode'])); ?></td>
+                                    </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+<?php include_once('layouts/footer.php'); ?>
--- warehouse-inventory System/warehouse-inventory system/includes/config.php
+++ warehouse-inventory System/warehouse-inventory system/includes/config.php
@@ -1,20 +1,22 @@
 <?php
 /*
 |--------------------------------------------------------------------------
-| OWSA-INV V2
+| TFM ERP
 |--------------------------------------------------------------------------
-| Author: Siamon Hasan
-| Project Name: OSWA-INV
-| Version: v2
-| Offcial page: http://oswapp.com/
-| facebook Page: https://www.facebook.com/oswapp
-|
-|
+| Author: Anush Mauranga
+| Project Name: TFM ERP System
+| Version: v1
 |
 */
-  define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '2454' );             // Set database password
-  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+
+  define( 'DB_HOST', '216.172.176.35' );    // Set database host
+  define( 'DB_USER', 'tfm_tfmdb' );        // Set database user
+  define( 'DB_PASS', 'Blackice#1977' );   // Set database password
+  define( 'DB_NAME', 'tfm_ERP' );        // Set database name
+
+//define( 'DB_HOST', 'localhost' );    // Set database host
+//define( 'DB_USER', 'root' );        // Set database user
+//define( 'DB_PASS', '2454' );   // Set database password
+//define( 'DB_NAME', 'oswa_inv' );        // Set database name
 
 ?>
